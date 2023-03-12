7d63c90b44fcd6cdf5c1671d91c4c0971a7e9ee9
Akandra-Pawitra <akandrapawitra@gmail.com>
2023-03-12 11:50:30 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './stylesheets/css/App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <nav>Navigation</nav>
+      </header>
+      <main>
+        Content
+      </main>
+    </div>
+  )
+}
+
+export default App
